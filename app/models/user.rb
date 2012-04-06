00223108f879edd939796ab2ec60e942ceafa167
Paul <paul.fletcher-hill@yale.edu>
2012-04-06 16:29:16 -0400

    Adding email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	validates :name, :presence => true
 	validates :email, :presence => true
+	validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
 	validates :year, :presence => true
 end
