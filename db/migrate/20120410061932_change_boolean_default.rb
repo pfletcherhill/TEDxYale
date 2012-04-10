c00cb4988893625fa15acc214d3a42201c07e0ba
Paul <paul.fletcher-hill@yale.edu>
2012-04-10 09:34:58 -0400

    Editing migration

--- db/migrate/20120410061932_change_boolean_default.rb
+++ db/migrate/20120410061932_change_boolean_default.rb
@@ -1,7 +1,7 @@
 class ChangeBooleanDefault < ActiveRecord::Migration
   def change
     change_table :users do |t|
-      t.remove :status
+      #t.remove :status
       t.boolean :status, :default => false
     end
   end
