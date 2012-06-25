1951eb144c62f0e8bdd13c3b0ab684b4a89174f9
Paul <paul.fletcher-hill@yale.edu>
2012-06-25 01:34:08 -0400

    updating migrations

--- db/migrate/20120402025125_create_users.rb
+++ db/migrate/20120402025125_create_users.rb
@@ -1,56 +1,75 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :team
-	    t.string :name
-  	  t.string :email
-  	  t.string :year
-  	  t.text :bio
-  	  t.text :ted_talk
-  	  t.string :speaker
-  	  t.text :question
-  	  t.text :experience
-  	  t.boolean :facebook
-  	  t.boolean :twitter
-  	  t.boolean :tumblr
-  	  t.boolean :google
-  	  t.boolean :final_cut
-  	  t.boolean :photoshop
-  	  t.boolean :lights
-  	  t.boolean :sound
-  	  t.boolean :video
-  	  t.boolean :html
-  	  t.boolean :css
-  	  t.boolean :jquery
-  	  t.boolean :javascript
-  	  t.boolean :ror
-  	  t.boolean :in_design
-  	  t.boolean :illustrator
-  	  t.boolean :exec
-  	  t.boolean :social_media_director
-  	  t.boolean :marketing_director
-  	  t.boolean :social_director
-  	  t.boolean :developer
-  	  t.boolean :designer
-  	  t.boolean :conference_director
-  	  t.boolean :salons_director
-  	  t.boolean :speakers_director
-  	  t.boolean :finance_director
-  	  t.boolean :fundraising_director
-  	  t.boolean :stage_manager
-  	  t.boolean :set_designer
-  	  t.boolean :lights_sound_director
-  	  t.boolean :video_director
-  	  t.boolean :photographer
-	  
+    create_table :attendees do |t|
+      t.integer :user_id
+      t.integer :event_id
+      t.boolean :checked_in, :default => false
+      
       t.timestamps
     end
-    
-    create_table :admins do |t|
-      t.string :email
+
+    create_table :events do |t|
       t.string :name
+      t.text :description
+      t.string :location
+      t.string :location_url
+      t.string :image
+      t.string :location_address
+      t.string :location_city
+      t.string :location_state
+      t.string :location_zipcode
+      t.boolean :published, :default => false
+      t.string :header_file_name
+      t.string :header_content_type
+      t.integer :header_file_size
+      t.datetime :header_updated_at
+      t.string :thumbnail_file_name
+      t.string :thumbnail_content_type
+      t.integer :thumbnail_file_size
+      t.datetime :thumbnail_updated_at
+      t.string :sort
+      
+      t.timestamps
+    end
+
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      
+      t.timestamps
+    end
+
+    create_table :presenters do |t|
+      t.integer :speaker_id
+      t.integer :event_id
+      
+      t.timestamps
+    end
+
+    create_table :speakers do |t|
+      t.string :name
+      t.text :bio
+      t.string :image_url
+      t.string :twitter
+      
+      t.timestamps
+    end
+
+    create_table :subscriptions do |t|
+      t.string :name
+      t.string :email
+      
+      t.timestamps
+    end
+
+    create_table :users do |t|
+      t.string :name
+      t.string :email
       t.string :password_hash
       t.string :password_salt
+      t.string :affiliation
+      t.string :admin, :limit => 1
+      t.string :year
       
       t.timestamps
     end
