77dfc34f3d5e88f1d4dd3883d7a4c1d4ab81b3a1
Paul <paul.fletcher-hill@yale.edu>
2012-09-02 14:05:17 -0400

    form...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,8 +35,8 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to '/signup' }
         format.json { render json: @user, status: :created, location: @user }
-        #UserMailer.welcome_email(@user).deliver
-        UserMailer.notify_admin_email(@user).deliver
+        UserMailer.welcome_email(@user).deliver
+        #UserMailer.notify_admin_email(@user).deliver
       else
         format.html
         format.json { render json: @user.errors, status: :unprocessable_entity }
