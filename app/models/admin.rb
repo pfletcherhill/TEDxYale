7e7b7aedf9f03075144374e626fd485ac8605afe
Paul <paul.fletcher-hill@yale.edu>
2013-09-03 20:49:38 -0400

    Stop using validates_format_of

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,9 +6,8 @@ class Admin < ActiveRecord::Base
 		
 	validates :name, :presence => true	
 	validates_confirmation_of :password  
-  	validates_presence_of :password, :on => :create  
+  validates_presence_of :password, :on => :create  
 	validates_presence_of :email 
-	validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
 	validates_uniqueness_of :email  
 		
 	def encrypt_password  
